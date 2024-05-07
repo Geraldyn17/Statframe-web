166fd240704d2f922a8cec638e30149f4f78d183
reiauyama <leosanmo_01-@outlook.es>
2024-05-06 21:14:02 -0500

    Se empezo el componente de Home

--- FRONT/src/components/layouts/pages/Home.jsx
+++ FRONT/src/components/layouts/pages/Home.jsx
@@ -1,13 +1,39 @@
 import NavigationBar from "../../helpers/Nav"
 import Footer from "../../helpers/Footer"
+import "./Home.css"
+import iconSF from "../../../assets/StatWIcon.png"
+import iosIcon from "../../../assets/ios.png"
+import andoroidIcon from "../../../assets/android.png"
 
 const Home = () => {
     return (
         <div>
-            <nav><NavigationBar/></nav>
-            <footer><Footer/></footer>  
+            <nav><NavigationBar /></nav>
+            <div className="homeMainContainer">
+                <div className=" mainInfo">
+                    <img className="SFicon" src={iconSF} alt="" />
+                    <p>
+                        StatFrame permite visualizar desde el telefono movil estadísticas, <br /> como las probabilidades de obtención de objetos, misiones en curso <br />como fisuras del vacío, estado del mundo como ciclos de Cetus, Fortuna, <br /> Deimos y más de Warframe ademas de recibir notificaciones de las <br /> rotaciones deseadas
+                    </p>
+                    <div className="downloandSF">
+                        <div className="downloadbtn">
+                            <img className="downloadIcon" src={andoroidIcon} alt="" />
+                            <h3>Descarga para Android</h3>
+                        </div> 
+                        <div className="downloadbtn">
+                            <img className="downloadIcon" src={iosIcon} alt="" />
+                            <h3>Descarga para iOs</h3>
+                        </div> 
+                            
+
+                        
+                      
+                    </div>
+                </div>
+            </div>
+            <footer><Footer /></footer>
         </div>
-        
+
     )
 
 }
